61ffd5461088ba77a7830151bd5d35ef5efa091c
fraret <fraretblauet@gmail.com>
2016-10-15 20:58:56 +0200

    Primer intent de afegir compensacio de potencia

--- cotxe_teledirigit/codis.h
+++ cotxe_teledirigit/codis.h
@@ -1,18 +1,41 @@
-/* Filtxer 
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+/* GPL v3 fraret <fraretblauet@gmail.com>
+ * 2015-2016
+ *
+ * FITXER DE CONFIGURACUIO DE cotxe_teledirigit.ino
  */
 
 
 #ifndef CODIS_H
 #define CODIS_H
 
-#define LT_MOTOR 12
-#define RT_MOTOR 13
-#define CODE_LT 0x4EBA0422
-#define CODE_RT 0x5EA08104
-#define CODE_FW 0x9986485A
-#define CODE_REPEAT 0x4AB0F7B6
-#define CICLES 20000
-#define RECV_PIN 11
+#define LT_MOTOR 12 //Pin del motor esquerre
+#define RT_MOTOR 13 //Pin del motor dret
+#define LT_POWER 255 //Potencia del motor esquerre (de 0 a 255)
+#define RT_POWER 255 //Potencia del motor esquerre (de 0 a 255)
+
+#define CODE_LT 0x4EBA0422 //Codi per anar cap a l'esquerra
+#define CODE_RT 0x5EA08104 //Codi per anar cap a la dreta
+#define CODE_FW 0x9986485A //Codi per anar endavant
+#define CODE_REPEAT 0xFFFFFFF //Codi de repetici del comandament (si en te)
+
+#define CICLES 20000 //Quants cicles ha de passar sense senyal per tal de que el cotxe s'aturi?
+
+#define RECV_PIN 11 //Pin del receptor de IR
+
 
 #endif
--- cotxe_teledirigit/cotxe_teledirigit.ino
+++ cotxe_teledirigit/cotxe_teledirigit.ino
@@ -1,5 +1,20 @@
 
-/* GPL v3 fraret 
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* GPL v3 fraret <fraretblauet@gmail.com>
  * 2015-2016
  * 
  * Aquest codi font (escrit per ser compilat per a Arduino) serveix per 
@@ -9,6 +24,8 @@
  * NO HAURIA DE SER NECESSARI MODIFICAR AQUEST FITXER PER TAL DE 
  * FER FUNCIONAR EL COMANDAMENT D'IR
  */
+ 
+ 
 #include <IRremote.h>
 #include "codis.h"
 
@@ -24,49 +41,50 @@ decode_results results;
 
 void setup()
 {
-  irrecv.enableIRIn(); //iniciar sensor IR
-  pinMode(LT_MOTOR, OUTPUT); 
-  digitalWrite(LT_MOTOR, LOW); 
-  pinMode(RT_MOTOR, OUTPUT); 
-  digitalWrite(RT_MOTOR, LOW); 
-  delay(200); //
+    irrecv.enableIRIn(); //iniciar sensor IR
+    pinMode(LT_MOTOR, OUTPUT); 
+    digitalWrite(LT_MOTOR, LOW); 
+    pinMode(RT_MOTOR, OUTPUT); 
+    digitalWrite(RT_MOTOR, LOW); 
+    delay(200); //
 }
 
 void loop()
 {
-  if (irrecv.decode(&results)) {
-    c = 0; //si rep alguna senyal, el contador de cicles sense senyal torna a zero
-    switch (results.value) { //tria la opcio que coincideixi amb el codi redut, si no executa la opcio "default"
-      case CODE_LT: 
-        digitalWrite(RT_MOTOR, HIGH); //engega el motor dret per tal de que el cotxe giri cap a l'esquerra
-        delay(100);
-        break;
-      case CODE_RT: //engega el motor esquerre per tal de que el cotxe giri cap a la dreta
-       digitalWrite(LT_MOTOR, HIGH);
-       delay(100);
-        break;
-      case CODE_FW: //engega els dos motors, per fer que el cotxe es mogui endavant
-      digitalWrite(LT_MOTOR, HIGH);
-      digitalWrite(RT_MOTOR, HIGH);
-        delay(100);
-        break;
-      case CODE_REPEAT: 
-        break; //si rep el codi de repeticio no fa res, ni apaga ni engega
-      default:
-        digitalWrite(LT_MOTOR, LOW);
-        digitalWrite(RT_MOTOR, LOW); // si rep algun altre codi apaga els motors
-        break;
+    if (irrecv.decode(&results)) {
+        c = 0; //si rep alguna senyal, el contador de cicles sense senyal torna a zero
+        switch (results.value) { //tria la opcio que coincideixi amb el codi redut, si no executa la opcio "default"
+        case CODE_LT: 
+            digitalWrite(RT_MOTOR, HIGH); //engega el motor dret per tal de que el cotxe giri cap a l'esquerra
+            delay(100);
+            break;
+        case CODE_RT: //engega el motor esquerre per tal de que el cotxe giri cap a la dreta
+            digitalWrite(LT_MOTOR, HIGH);
+            delay(100);
+            break;
+        case CODE_FW: //engega els dos motors, per fer que el cotxe es mogui endavant
+            analogWrite(LT_MOTOR, LT_POWER);
+            analogWrite(RT_MOTOR, RT_POWER);
+            delay(100);
+            break;
+        case CODE_REPEAT: 
+            break; //si rep el codi de repeticio no fa res, ni apaga ni engega
+        default:
+            digitalWrite(LT_MOTOR, LOW);
+            digitalWrite(RT_MOTOR, LOW); // si rep algun altre codi apaga els motors
+            break;
+        }
+        irrecv.resume();
     }
-    irrecv.resume();
-  }
-  else { //si no rep senyal, espera CICLES cicles, perque el IR no pot rebre i descodificar una senyal cada cicle
-    c = c + 1;
-    if(c > CICLES){ //si al cap de CICLES cicles no capta cap codi, apaga els dos motors
-      digitalWrite(LT_MOTOR, LOW);
-      digitalWrite(RT_MOTOR, LOW);
+    else { //si no rep senyal, espera CICLES cicles, perque el IR no pot rebre i descodificar una senyal cada cicle
+        c = c + 1;
+        if(c > CICLES){ //si al cap de CICLES cicles no capta cap codi, apaga els dos motors
+            digitalWrite(LT_MOTOR, LOW);
+            digitalWrite(RT_MOTOR, LOW);
+        }
     }
-  }
 }
+
 
 
 
